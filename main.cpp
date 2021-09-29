2ac7c6cd77153fd52eca60d23b40ad4705155873
Kaur, Japneet <japkaur@iu.edu>
2021-09-29 00:07:42 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -49,12 +49,12 @@ int main (int argc, char * argv [])
 
 // COMMENT: Instead of using C functions to implement parts of the
 // calculator. It would be better suited to use a Wrapper Facade.
-// SOLUTION  Dr. Hill, I resolved this error by creating a wrapper facade
+// SOLUTION  Dr. Hill, I resolved this design error by creating a wrapper facade
 // class named "Calculate" which executes the task of parsing, converting
 // the infix to postfix, and evaluate the postfix. It further facilitates the 
 // calculation of the expression entered by the user by providing helper 
 // methods for getting the result, changing the infix value, etc. Hence, I
-// removed the those methods from here and included their implementation in
+// removed those methods from here and included their implementation in the
 // facade class.
 
 // PLEASE NOTE: The following was a comment provided by Dr. Hill in my Open and Closed Paranthesis 
@@ -67,6 +67,6 @@ int main (int argc, char * argv [])
 // While doing that, I also realized that the only reason I had created Open Paranthesis
 // and Closed Paranthesis Objects was to keep track of their precedence value to distinguish
 // them. This could have been done using the Expr_Command class i.e. the base of all classes.
-// Since, I was a partially abstract class, and not a pure abstract class, it could've been
-// instntiated. Hence, I created an instance of that to keep track of the precedence and managed
+// Since, It was a partially abstract class, and not a pure abstract class, it could've been
+// instantiated. Hence, I created an instance of that to keep track of the precedence and managed
 // the parantheses that way.
